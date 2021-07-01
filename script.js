f5e09f87cd1b2db6af10713856f79eab8ba85ddf
Tenki2 <86297701+Tenki2@users.noreply.github.com>
2021-07-01 15:08:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+var t1;
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Briston&appid=a93533fbcacec5513e231ee77af73e97')
     .then(response => response.json())
-    .then(function (response){
-        let t1 = response.title
+    .then(response => {
+        console.log(response)
+        console.log(response.weather[0].description)
+        t1 = response.weather[0].description
+        console.log(t1)
+        document.getElementById("test").innerHTML = "the weather in Briston is " + t1
+        return t1
     })
+let t2 = t1;
+function newtext(t2) {
 
-function newtext(t1) {
-
-    document.getElementById("test").innerHTML = t1
+    document.getElementById("test").innerHTML = "gone"
 
 
 }
